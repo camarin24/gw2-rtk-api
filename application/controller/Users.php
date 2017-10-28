7507ef4df7d4647be294c3f513f05349a585234a
Cristian Marin <cm961224@gmail.com>
2017-10-28 11:23:37 -0500

    first commit

---
+++ application/controller/Users.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+class Users extends Controller {    
+    private $mdlModel=null;
+
+    function __construct(){
+        $this->mdlModel = $this->loadModel("mdlUsers");
+    }
+
+    public function GetAll(){ 
+        echo json_encode($this->mdlModel->GetAll());
+    }
+
+    public function Login(){
+        $data = json_decode(file_get_contents('php://input')."");
+        $user = $this->mdlModel->Login($data->UserName,$data->Password);
+        if($user){
+            $user['Token'] = $this->crypt($user['UserId'],'e');
+            echo json_encode($user);
+        }else{
+            header($_SERVER["SERVER_PROTOCOL"]." 401 Unautorized", true, 401);
+        }
+    }
+}
